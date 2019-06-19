094bba69a45f0d43b7cb7d4b4cdcdffb38383d62
madanjadhav <jadhavmadan25@gmail.com>
2019-06-19 09:11:46 +0530

    add to new branch

--- src/com/cjc/test/Test.java
+++ src/com/cjc/test/Test.java
@@ -37,6 +37,29 @@ public class Test {
 		e1.setDepartment(d1);
 		e2.setDepartment(d1);
 		e3.setDepartment(d1);
+
+Employee e1=new Employee();
+		e1.setEmp_name("Madan");
+		e1.setAddress("Satara");
+		
+		
+		Employee e2=new Employee();
+		e2.setEmp_name("Amit");
+		e2.setAddress("Wai");
+		
+		Employee e3=new Employee();
+		e3.setEmp_name("Ravi");
+		e3.setAddress("Ambegaon");
+		
+		Department d1=new Department();
+		d1.setDep_name("IT-DEP");
+		d1.getEmployee().add(e1);
+		d1.getEmployee().add(e2);
+		d1.getEmployee().add(e3);
+		
+		e1.setDepartment(d1);
+		e2.setDepartment(d1);
+		e3.setDepartment(d1);
 		
 		
 		session.save(d1);
